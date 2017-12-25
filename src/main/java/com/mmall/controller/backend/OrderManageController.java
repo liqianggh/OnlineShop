37b9e32f0d551579e1020258f03f722683cae968
LiQiang <liqiang1chn@163.com>
2017-12-25 18:41:19 +0800

    alipay_callback

---
+++ src/main/java/com/mmall/controller/backend/OrderManageController.java
@@ -0,0 +1,7 @@
+package com.mmall.controller.backend;
+
+/**
+ * Created by Administrator on 2017/12/24 0024.
+ */
+public class OrderManageController {
+}
---
+++ src/test/java/com/mmall/test/PropertiesUtilsTest.java
@@ -0,0 +1,7 @@
+package com.mmall.test;
+
+/**
+ * Created by Administrator on 2017/12/24 0024.
+ */
+public class PropertiesUtilsTest {
+}
